d2fd10ad3cc8839eb0a3bc587d522fc5c2b0813c
Carolina <krito_j_g@utp.edu.co>
2017-05-23 12:11:38 -0500

    Agrega texto botones, helpers, objects, imports

--- game.py
+++ game.py
@@ -1,30 +1,52 @@
 import pygame
 import random
+from imports import *
+from helpers import *
+from objects import *
 
-WIDTH = 1024
+# Valores para la pantalla
+
+WIDTH = 1024 + 300
 HIGH = 700
 
 if __name__ == '__main__':
 	pygame.init()
-	SCREEN = pygame.display.set_mode([WIDTH,HIGH])
-	picture = pygame.image.load('Background.png')
-	picture = pygame.transform.scale(picture, (WIDTH, HIGH))
-	rect = picture.get_rect()
-	SCREEN.blit(pygame.transform.scale(picture, (WIDTH, HIGH)), (0, 0))
-	pygame.display.flip()
-
 	reloj = pygame.time.Clock()
-	pygame.display.set_caption("Parques Distribuido")
-	background = pygame.image.load('Background.png').convert()
-
+	fuente = pygame.font.SysFont("comicsansms", 30)
+	# pantalla_s=load_sound('background.ogg',curdir)
 	pos = pygame.mouse.get_pos()#recibe donde esta ubicado el mouse
+
+	#----------------SCREEN--------------------------
+
+	SCREEN = pygame.display.set_mode([WIDTH,HIGH])
+	bg = pygame.image.load('Background.png')
+	bg = pygame.transform.scale(bg, (WIDTH-300, HIGH))
+	rect = bg.get_rect()
+	pygame.display.set_caption("Parques Distribuido")
+	SCREEN.fill((0,0,0))
+
+	#--------------BOTONES---------------------------
+
+	txt_btn_iniciar = fuente.render("INICIAR JUEGO",1, BLANCO)
+	txt_btn_salir = fuente.render("SALIR",1, BLANCO)
+	txt_btn_tirar = fuente.render("TIRAR",1, BLANCO) # tirar dados
+	btn_x = WIDTH
+	btn_y = 130
+	btn_deltax = -250
+	btn_deltay = 100
+
+	btn_iniciar = Cuadro(SCREEN,100,100,VERDE)
+
+	#---------------Sprites--------------------------
+
 	ls_all = pygame.sprite.Group()
 	ls_impactos = pygame.sprite.Group()
+
 	#----------------JUGADOR-------------------------
 
 
     #------------------------------------------------
-	pygame.display.flip()
+
 	end = False
 
 	while not end:
@@ -40,6 +62,10 @@ if __name__ == '__main__':
         #------------IMPRIME EN PANTALLA--------------
 		#ls_all.update()
 		#ls_all.draw(SCREEN)
-		SCREEN.blit(pygame.transform.scale(picture, (WIDTH, HIGH)), (0, 0))
+		SCREEN.blit(pygame.transform.scale(bg, (WIDTH-300, HIGH)), (0, 0))
+		btn_iniciar.draw(bg,WIDTH+100,HIGH+100)
+		SCREEN.blit(txt_btn_iniciar,(btn_x + btn_deltax, btn_y))
+		SCREEN.blit(txt_btn_salir,(btn_x + btn_deltax, btn_y + btn_deltay))
+		SCREEN.blit(txt_btn_tirar,(btn_x + btn_deltax, btn_y + 2*btn_deltay))
 		pygame.display.flip()
 		reloj.tick(60)
