2c7b7bf24f74f57f32052726dd69121f3aa8b29b
germang <germandg99@gmail.com>
2017-05-23 15:11:42 -0500

    Se agrega boton dados y salir a pantalla de juego

--- parchis/game.py
+++ parchis/game.py
@@ -16,8 +16,7 @@ if __name__ == '__main__':
 	reloj = pygame.time.Clock()
 	fuente = pygame.font.SysFont("comicsansms", 30)
 	# pantalla_s=load_sound('background.ogg',curdir)
-	pos = pygame.mouse.get_pos()#recibe donde esta ubicado el mouse
-	cursor= Cursor()
+	cursor = Cursor()
 
 	##############################################
 	# muestra el menu
@@ -36,10 +35,18 @@ if __name__ == '__main__':
 		#--------------BOTONES---------------------------
 
 		
-		btn_x = WIDTH
-		btn_y = 130
-		btn_deltax = -250
-		btn_deltay = 100
+		margen_x_dados = WIDTH - 220
+		margen_y_dados = 300
+		margen_x_salir = WIDTH - 220
+		margen_y_salir = HIGH - 100
+		btn_dados1 = pygame.image.load("objetos/dados.png")
+		btn_dados2 = pygame.image.load("objetos/dados2.png")
+		btn_salir1= pygame.image.load("objetos/botones/2.png")
+		btn_salir2= pygame.image.load("objetos/botones/2.2.png")
+		btn_dados1 = pygame.transform.scale(btn_dados1, (100, 100))
+		btn_dados2 = pygame.transform.scale(btn_dados2, (100, 100))
+		boton_salir = Boton(btn_salir1,btn_salir2,margen_x_salir,margen_y_salir)
+		boton_dados = Boton(btn_dados1, btn_dados2, margen_x_dados, margen_y_dados)
 
 		#---------------Sprites--------------------------
 
@@ -66,8 +73,11 @@ if __name__ == '__main__':
 	        #------------IMPRIME EN PANTALLA--------------
 			#ls_all.update()
 			#ls_all.draw(SCREEN)
+			cursor.posicion()
+			SCREEN.fill((0,0,0))
+			boton_salir.accion(SCREEN, cursor)
+			boton_dados.accion(SCREEN, cursor)
 			SCREEN.blit(pygame.transform.scale(bg, (WIDTH-300, HIGH)), (0, 0))
-
 			pygame.display.flip()
 			reloj.tick(60)
 		else:
--- parchis/objects.py
+++ parchis/objects.py
@@ -11,21 +11,22 @@ class Ficha(pygame.sprite.Sprite):
 class Cursor(pygame.Rect):
     def __init__(self):
         pygame.Rect.__init__(self,0,0,1,1)
+    
     def posicion(self):
-        self.left,self.top=pygame.mouse.get_pos()
+        self.left,self.top = pygame.mouse.get_pos()
 
 
 class Boton(pygame.sprite.Sprite):
     def __init__(self,imagen1, imagen2,x,y):
-        self.imagen_normal= imagen1
-        self.imagen_seleccion= imagen2
-        self.imagen_actual=self.imagen_normal
-        self.rect=self.imagen_actual.get_rect()
-        self.rect.left,self.rect.top=x,y
+        self.imagen_normal = imagen1
+        self.imagen_seleccion = imagen2
+        self.imagen_actual = self.imagen_normal
+        self.rect = self.imagen_actual.get_rect()
+        self.rect.left,self.rect.top = x,y
 
     def accion(self,pantalla,cursor):
         if cursor.colliderect(self.rect):
-            self.imagen_actual= self.imagen_seleccion
+            self.imagen_actual = self.imagen_seleccion
         else:
-            self.imagen_actual=self.imagen_normal
+            self.imagen_actual = self.imagen_normal
         pantalla.blit(self.imagen_actual,self.rect)
